311563bc47d7ce9993d35de23cf724641c3edba0
jamestang12 <homing.tang@ryerson.ca>
2020-05-04 17:28:54 -0400

    Tech Options Select Components

--- src/compoments/Logs/AddLogModal.js
+++ src/compoments/Logs/AddLogModal.js
@@ -3,6 +3,7 @@ import M from "materialize-css/dist/js/materialize.min.js";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import { addLog } from "../../actions/logActions";
+import TechSelectOptions from "../techs/TechSelectOptions";
 
 export const AddLogModal = ({ addLog }) => {
   const [message, setMessage] = useState("");
@@ -58,9 +59,7 @@ export const AddLogModal = ({ addLog }) => {
               onChange={(e) => setTech(e.target.value)}
             >
               <option value="">Select Technician</option>
-              <option value="John Doe">John Doe</option>
-              <option value="Sam Smith">Sam Smith</option>
-              <option value="Sara Wilson">Sara Wilson</option>
+              <TechSelectOptions />
             </select>
           </div>
         </div>
--- src/compoments/Logs/EditLogModal.js
+++ src/compoments/Logs/EditLogModal.js
@@ -3,6 +3,7 @@ import M from "materialize-css/dist/js/materialize.min.js";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import { updateLog } from "../../actions/logActions";
+import TechSelectOptions from "../techs/TechSelectOptions";
 
 export const EditLogModal = ({ current, updateLog }) => {
   const [message, setMessage] = useState("");
@@ -63,9 +64,7 @@ export const EditLogModal = ({ current, updateLog }) => {
               onChange={(e) => setTech(e.target.value)}
             >
               <option value="">Select Technician</option>
-              <option value="John Doe">John Doe</option>
-              <option value="Sam Smith">Sam Smith</option>
-              <option value="Sara Wilson">Sara Wilson</option>
+              <TechSelectOptions />
             </select>
           </div>
         </div>
