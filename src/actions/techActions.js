8e3a567c5cc388b7a923cbf1ae85dbfb49bdaec1
jamestang12 <homing.tang@ryerson.ca>
2020-05-05 17:30:03 -0400

    Delete Technicians

--- src/actions/techActions.js
+++ src/actions/techActions.js
@@ -6,9 +6,7 @@ import {
   TECHS_ERROR,
 } from "./types";
 
-//Get techs from server
-
-//Get logs from server
+//Get technicians from server
 export const getTechs = () => async (dispatch) => {
   try {
     setLoading();
@@ -19,6 +17,27 @@ export const getTechs = () => async (dispatch) => {
     dispatch({
       type: GET_TECHS,
       payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: TECHS_ERROR,
+      payload: error.response.statusText,
+    });
+  }
+};
+
+//Delete technician from server
+export const deleteTech = (id) => async (dispatch) => {
+  try {
+    setLoading();
+
+    await fetch(`/techs/${id}`, {
+      method: "DELETE",
+    });
+
+    dispatch({
+      type: DELETE_TECH,
+      payload: id,
     });
   } catch (error) {
     dispatch({
